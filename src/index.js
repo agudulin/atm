268da198d29d8bac10e895d4deb9f3daa7a0a563
Alexander Gudulin <alexandr.gudulin@gmail.com>
2016-08-28 12:13:50 +0200

    fix NPE when no dev tools available

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import { Provider } from 'react-redux'
 import rootReducer from 'common/reducer'
 import App from './app'
 
-const devTools = window.devToolsExtension && window.devToolsExtension()
+const enhancers = [applyMiddleware(thunk)]
+if (window.devToolsExtension) {
+  enhancers.push(window.devToolsExtension())
+}
 // create a store with enhancers:
 //  - thunk: to have an ability to return functions from actions, not just plain objects
-//  - devTools: to help development of redux app
-const store = createStore(rootReducer, {}, compose(applyMiddleware(thunk), devTools))
+//  - devTools: to help with development of redux app
+const store = createStore(rootReducer, {}, compose(...enhancers))
 
 // mount react app to the DOM node
 ReactDOM.render(
